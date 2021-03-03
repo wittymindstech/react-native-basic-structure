2573af972a3cc8b8d958885311d0dc4db9bb6d62
gauravshremayee <gaurav.2897@gmail.com>
2021-03-03 23:12:52 +0530

    react native basic project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+import {HelloWorld} from '_atoms';
+import Navigator from '_navigations';
+
+const App = () => <Navigator />;
+
+
+const App = () => (
+  <View>
+    <Text>Hello World</Text>
+  </View>
+);
+
+export default App;
